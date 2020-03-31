93ee3152fa7d243332d99788b721d976318f4f41
Ezwal <ezwal@github.com>
2020-03-31 18:13:16 +0200

    more consistent downloading and adding 4k dl support

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ if (process.argv.length < 3) {
 }
 
 const collectionId = process.argv[2];
+const is4k = process.argv[3] === '4k';
 
 const getCollectionUrl = id => page => `https://www.artstation.com/collections/${id}/projects.json?collection_id=${id}&page=${page}`;
 const fetchJsonPage = async url => (await axios.get(url)).data;
 
 const sanitizeForFs = s => s.replace(/[\\\/?"!<>:|?*]/gi, ''); // for both windows and linux...
 const getLargeArtwork = s => s
-      .replace('/small_square/', '/large/')
+      .replace('/small_square/', is4k ? '/4k/' : '/large/')
       .replace(/\d{14}\//, '');
 const pickArtworkData = pageArtwork => pageArtwork.data.map(singleArtwork => ({
     title: `${sanitizeForFs(singleArtwork.title)} - ${sanitizeForFs(singleArtwork.user.username)}`,
@@ -38,17 +39,37 @@ const drainWriter = writable => {
             .then(() => Promise.reject(new Error('premature close')))
     ]);
 }
+
+const downloadArtwork = async (title, link) => {
+    const res = await axios({
+        url: link,
+        method: 'get',
+        responseType: 'stream',
+    });
+    const fileWriter = res.data.pipe(fs.createWriteStream(`${downloadDir}/${title}.jpg`));
+    await drainWriter(fileWriter);
+}
 const downloadAll = async artworksInfo => {
     await fs.promises.mkdir(downloadDir, { recursive: true });
-    artworksInfo.forEach(async ({title, link}) => {
-        const res = await axios.get(link, {responseType: 'stream'});
-        const fileWriter = res.data.pipe(fs.createWriteStream(`${downloadDir}/${title}.jpg`))
+    artworksInfo.reduce(async (prevDownload, {title, link}) => {
+        await prevDownload;
+        console.log(`[log] ${title} from ${link}`);
 
-        await drainWriter(fileWriter);
-    })
+        let tryCount = 3;
+        let artworkDrain;
+        while (tryCount > 0) {
+            try {
+                artworkDrain = await downloadArtwork(title, link);
+                break;
+            } catch (err) {
+                console.error(`[err] ${title} downloading failure, tryCount ${tryCount}`)
+                tryCount -= 1;
+            }
+        }
+    }, Promise.resolve());
 }
 
-const getFirstArtwork = async url => {
+const getFirstArtwork = async (url, is4k = false) => {
     const getCollectionPage = getCollectionUrl(url);
     console.log('collection url ', getCollectionPage(1), 'usw...');
 
