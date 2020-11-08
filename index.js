366405cd0887a3bd9d05e22ea4dd060f212b1c8d
Ezwal <ezwal@github.com>
2020-11-08 17:17:09 +0100

    Add filter features

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ const process = require('process');
 const axios = require('axios');
 
 if (process.argv.length < 3) {
-    console.error('missing collection id');
+    console.log(`Usage: node index.js COLLECTION_ID [4k] [diff]
+   4k - try to download the artwork in 4k if possible
+   diff - if artwork already exists then dont dl it
+`);
     process.exit(1);
 }
 
+const fourk = '4k';
+const diff = 'diff'
 const collectionId = process.argv[2];
-const is4k = process.argv[3] === '4k';
+const is4k = process.argv.find(el => el === fourk) || false
+const diffMode = process.argv.find(el => el === diff) || false
 
 const getCollectionUrl = id => page => `https://www.artstation.com/collections/${id}/projects.json?collection_id=${id}&page=${page}`;
 const fetchJsonPage = async url => (await axios.get(url)).data;
 
-const sanitizeForFs = s => s.replace(/[\\\/?"!<>:|?*]/gi, ''); // for both windows and linux...
+const SANITIZE_REGEX = /[\\\/?"!<>:|?*]/gi
+const sanitizeForFs = s => s.replace(SANITIZE_REGEX, ''); // for both windows and linux...
 const getLargeArtwork = s => s
       .replace('/small_square/', is4k ? '/4k/' : '/large/')
       .replace(/\d{14}\//, '');
@@ -26,7 +33,7 @@ const pickArtworkData = pageArtwork => pageArtwork.data.map(singleArtwork => ({
     title: `${sanitizeForFs(singleArtwork.title)} - ${sanitizeForFs(singleArtwork.user.username)}`,
     link: getLargeArtwork(singleArtwork.cover.small_square_url),
     assets_count: singleArtwork.assets_count,
-}));
+})).filter(artwork => !diffMode || isDownloaded(artwork.title));
 
 const downloadDir = './artworks'
 const drainWriter = writable => {
@@ -49,27 +56,36 @@ const downloadArtwork = async (title, link) => {
     const fileWriter = res.data.pipe(fs.createWriteStream(`${downloadDir}/${title}.jpg`));
     await drainWriter(fileWriter);
 }
+
 const downloadAll = async artworksInfo => {
-    await fs.promises.mkdir(downloadDir, { recursive: true });
     artworksInfo.reduce(async (prevDownload, {title, link}) => {
         await prevDownload;
-        console.log(`[log] ${title} from ${link}`);
+        console.log(`[dl] ${title} from ${link}`);
 
         let tryCount = 3;
-        let artworkDrain;
         while (tryCount > 0) {
             try {
-                artworkDrain = await downloadArtwork(title, link);
+                await downloadArtwork(title, link);
                 break;
             } catch (err) {
-                console.error(`[err] ${title} downloading failure, tryCount ${tryCount}`)
+                console.error(`[dl] ${title} download failure, tryCount: ${tryCount}`)
                 tryCount -= 1;
             }
         }
     }, Promise.resolve());
 }
 
-const getFirstArtwork = async (url, is4k = false) => {
+const exceptFileType = fileName => fileName.split('.')[0]
+let downloadedArtworks = []
+const isDownloaded = artworkTitle => downloadedArtworks
+      .find(dlTitle => dlTitle === artworkTitle)
+
+const fsPreOp = async () => {
+        await fs.promises.mkdir(downloadDir, { recursive: true });
+        downloadedArtworks = (await fs.promises.readdir(downloadDir))
+            .map(title => exceptFileType(title))
+}
+const getArtworks = async url => {
     const getCollectionPage = getCollectionUrl(url);
     console.log('collection url ', getCollectionPage(1), 'usw...');
 
@@ -85,8 +101,8 @@ const getFirstArtwork = async (url, is4k = false) => {
             allArtworks = allArtworks.concat(filteredArtworkInfo);
             pageCount += 1;
         }
+        console.log('artworks nb:', allArtworks.length, 'page nb :', pageCount);
 
-        console.log('artworks nb :', allArtworks.length, 'page nb :', pageCount);
         await downloadAll(allArtworks)
     } catch (err) {
         console.error(err);
@@ -94,5 +110,6 @@ const getFirstArtwork = async (url, is4k = false) => {
 }
 
 (async () =>{
-    await getFirstArtwork(collectionId);
+    await fsPreOp()
+    await getArtworks(collectionId);
 })()
